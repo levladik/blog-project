91610925a01522b697ac6c162163f1d875bbaf46
Levladik <vladislavtyshenko@yandex.ru>
2025-04-21 17:22:49 +0400

    feat: add UserForm component for user creation with dialog interface

--- blog-frontend/src/App.tsx
+++ blog-frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { UserForm } from './components/UserForm';
 import { UserList } from './components/UserList';
 import './styles/App.css';
 
 function App() {
   return (
     <>
+      <UserForm />
       <UserList refreshTrigger />
     </>
   );
